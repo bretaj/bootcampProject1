6a2e63c19c24eeaaba8f3f7fd30030934ee647c7
Chris <cjswindle1971@gmail.com>
2024-12-14 08:27:00 -0800

    Minor Changes

--- JS/homepage.js
+++ JS/homepage.js
@@ -28,8 +28,6 @@ document.addEventListener('DOMContentLoaded', () => {
   
     // Add a click event on various child elements to close the parent modal
     // (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
-    //   const $target = $close.closest('.modal');
-  
     //   $close.addEventListener('click', () => {
     //     closeModal($target);
     //   });
